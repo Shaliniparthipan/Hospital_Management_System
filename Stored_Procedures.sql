b9ac32b13b87d36e41a8b24159d23b1a692a6980
Shaliniparthipan <888shalini0313@gmail.com>
2026-06-17 00:02:08 +0530

    Update Stored_Procedures.sql

--- Stored_Procedures.sql
+++ Stored_Procedures.sql
@@ -20,12 +20,16 @@ call getapp_detail();
 
 -- 83. Create a procedure to calculate total bill amount of a patient
 call gettotal_bill();
+CALL `hospitaldb`.`gettotal_bill`();
 
 --  84. Create a procedure to show all doctors in a department
 call doctor_list();
+delimiter ;CALL `hospitaldb`.`doctor_list`();
 
 -- 85. Create a procedure to display top 5 highest billed patients
 call top_patients();
+CALL `hospitaldb`.`top_patients`();
 
 -- 86. Create a procedure to count appointments of a doctor
 call total_app();
+CALL `hospitaldb`.`total_app`();
